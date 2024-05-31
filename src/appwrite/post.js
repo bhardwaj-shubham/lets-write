2179ae754d3b433286f2ba0bd7052fdc57112a45
Shubham Bhardwaj <shubhardwaj10@gmail.com>
2024-05-31 14:55:34 +0530

    feat: Add PostService class for managing posts

---
+++ src/appwrite/post.js
@@ -0,0 +1,189 @@
+import config from "@/config/config";
+import { Client, Databases, Storage, Query, ID } from "appwrite";
+
+export class PostService {
+	client = new Client();
+	databases;
+	bucket;
+	databaseId;
+	collectionId;
+	bucketId;
+
+	constructor() {
+		this.client
+			.setEndpoint(config.appwriteUrl)
+			.setProject(config.appwriteProjectId);
+		this.databases = new Databases(this.client);
+		this.bucket = new Storage(this.client);
+		this.databaseId = config.appwriteDatabaseId;
+		this.collectionId = config.appwriteCollectionId;
+		this.bucketId = config.appwriteBucketId;
+	}
+
+	// create a post
+	async createPost({ title, slug, content, featuredImage, status, userId }) {
+		try {
+			const response = await this.databases.createDocument(
+				this.databaseId,
+				this.collectionId,
+				slug,
+				{
+					title,
+					content,
+					featuredImage,
+					status,
+					userId,
+				}
+			);
+
+			if (response?.errors) {
+				throw new Error(response?.errors[0].message);
+			}
+
+			return response;
+		} catch (error) {
+			throw new Error(error);
+		}
+	}
+
+	// update post
+	async update(slug, { title, content, featuredImage, status }) {
+		try {
+			const response = await this.databases.updateDocument(
+				this.databaseId,
+				this.collectionId,
+				slug,
+				{
+					title,
+					content,
+					featuredImage,
+					status,
+				}
+			);
+
+			if (response?.errors) {
+				throw new Error(response?.errors[0].message);
+			}
+
+			return response;
+		} catch (error) {
+			throw new Error(error);
+		}
+	}
+
+	// delete post
+	async deletePost(slug) {
+		try {
+			const response = await this.databases.deleteDocument(
+				this.databaseId,
+				this.collectionId,
+				slug
+			);
+
+			if (response?.errors) {
+				throw new Error(response?.errors[0].message);
+			}
+
+			return response;
+		} catch (error) {
+			throw new Error(error);
+		}
+	}
+
+	// get post
+	async getPost(slug) {
+		try {
+			const response = await this.databases.getDocument(
+				this.databaseId,
+				this.collectionId,
+				slug
+			);
+
+			if (response?.errors) {
+				throw new Error(response?.errors[0].message);
+			}
+
+			return response;
+		} catch (error) {
+			throw new Error(error);
+		}
+	}
+
+	// get all posts
+	async getPosts(queries = [Query.equal("status", "active")]) {
+		try {
+			const response = await this.databases.listDocuments(
+				this.databaseId,
+				this.collectionId,
+				queries
+			);
+
+			if (response?.errors) {
+				throw new Error(response?.errors[0].message);
+			}
+
+			return response;
+		} catch (error) {
+			throw new Error(error);
+		}
+	}
+
+	// upload the featured image file
+	async uploadFile(file) {
+		try {
+			const response = await this.bucket.createFile(
+				this.bucketId,
+				ID.unique(),
+				file
+			);
+
+			if (response?.errors) {
+				throw new Error(response?.errors[0].message);
+			}
+
+			return response;
+		} catch (error) {
+			throw new Error(error);
+		}
+	}
+
+	// delete the uploaded featured image file
+	async deleteFeaturedImage(fileId) {
+		try {
+			const response = await this.bucket.deleteFile(
+				this.bucketId,
+				fileId
+			);
+
+			if (response?.errors) {
+				throw new Error(response?.errors[0].message);
+			}
+
+			return response;
+		} catch (error) {
+			throw new Error(error);
+		}
+	}
+
+	// get featured image file preview
+	async getFeaturedImagePreview(fileId) {
+		try {
+			const response = await this.bucket.getFilePreview(
+				this.bucketId,
+				fileId
+			);
+
+			if (response?.errors) {
+				throw new Error(response?.errors[0].message);
+			}
+
+			return response;
+		} catch (error) {
+			throw new Error(error);
+		}
+	}
+}
+
+const postService = new PostService();
+
+export default postService;
